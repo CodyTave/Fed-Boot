59c9471cf0e7f6cf26f716f4000adab364b2fa51
CodyTave <111206206+CodyTave@users.noreply.github.com>
2023-08-08 14:40:03 +0100

    Create Svg.tsx

--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -1,8 +1,9 @@
 interface props {
   src: string;
+  Class: string;
 }
-function Svg({ src }: props) {
-  return <object type="image/svg+xml" data={src}></object>;
+function Svg({ src, Class }: props) {
+  return <object className={Class} type="image/svg+xml" data={src}></object>;
 }
 
 export default Svg;
